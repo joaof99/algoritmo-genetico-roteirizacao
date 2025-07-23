ce8cce5c7a5f9073dbe60cd2026295f725bf25dc
João <Sem e-mail>
2025-07-22 21:34:52 -0300

    1- Terminando o básico de reprodução

--- src/main/java/com/genetico/Reproducao.java
+++ src/main/java/com/genetico/Reproducao.java
@@ -4,6 +4,10 @@ import com.genetico.model.Cromossomo;
 import com.genetico.model.Populacao;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Reproducao {
 
@@ -13,12 +17,16 @@ public class Reproducao {
         reproduzir();
     }
 
-    private static void reproduzir(){
+    private static void reproduzir() {
         var populacao = new Populacao();
         var contadorGeracoes = 0;
         var contadorPopulacao = 0;
 
-        var segundaPopulacao = new ArrayList<Cromossomo>();
+        var cromossomosFilhos = new ArrayList<Cromossomo>();
+
+        System.out.println("============= POPULAÇÃO INICIAL ===========================================");
+        populacao.imprimirPopulacao();
+        System.out.println("========================================= ===========================================");
 
         do {
             System.out.println("Geração: " + (contadorGeracoes + 1));
@@ -27,21 +35,38 @@ public class Reproducao {
                 var pai1 = populacao.selecionarCromossomoPaiPorRoleta();
                 var pai2 = populacao.selecionarCromossomoPaiPorRoleta();
 
-                segundaPopulacao.add(pai1);
-                segundaPopulacao.add(pai2);
+                List<Cromossomo> filhos = pai1.realizarCrossoverPmx(pai2, new Random());
+
+                cromossomosFilhos.add(filhos.get(0));
+                cromossomosFilhos.add(filhos.get(1));
 
                 contadorPopulacao++;
             } while (contadorPopulacao < Populacao.TAMANHO_POPULACAO / 2);
 
             contadorPopulacao = 0;
 
-            System.out.println("Imprimindo população de filhos, de tamanho: " + segundaPopulacao.size());
-            segundaPopulacao
-                    .forEach(cromossomo -> System.out.println(cromossomo.formatarGenes()));
+            System.out.println("Imprimindo população de filhos, de tamanho: " + cromossomosFilhos.size());
+            cromossomosFilhos.forEach(cromossomo -> System.out.println(cromossomo.formatarGenes()));
 
-            segundaPopulacao.clear();
+            var todosOsCromossomos = new ArrayList<>(populacao.getCromossomos());
+            todosOsCromossomos.addAll(cromossomosFilhos);
+
+            var melhoresCromossomosPopulacao = todosOsCromossomos
+                    .stream()
+                    .sorted(Comparator.comparing(Cromossomo::getFitness))
+                    .limit(Populacao.TAMANHO_POPULACAO)
+                    .collect(Collectors.toList());
+
+            populacao = new Populacao(melhoresCromossomosPopulacao);
+            System.out.println("População fusão quantidade: " + populacao.getCromossomos().size());
+
+            cromossomosFilhos.clear();
 
             contadorGeracoes++;
         } while (contadorGeracoes < QTDE_GERACOES);
+
+        System.out.println("População final: ");
+
+        populacao.imprimirPopulacao();
     }
 }
