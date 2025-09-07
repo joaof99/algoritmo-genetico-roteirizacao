d6297ca6bba92118472f712f0e398e5794c8cb35
João <Sem e-mail>
2025-09-07 10:30:22 -0300

    Incluindo final

--- src/main/java/com/genetico/model/Populacao.java
+++ src/main/java/com/genetico/model/Populacao.java
@@ -10,7 +10,7 @@ public class Populacao {
     private static final int PROBABILIDADE_OCORRER_CROSSOVER = 50;
     private static final int PROBABILIDADE_OCORRER_MUTACAO = 50;
     private final int tamanhoPopulacao;
-    private Random randomizador;
+    private final Random randomizador;
 
     public Populacao(int tamanhoPopulacao) {
         this.tamanhoPopulacao = tamanhoPopulacao;
