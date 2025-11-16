94743636a4371a55bcb17ef0c8f8207f5fc20e6f
João <Sem e-mail>
2025-11-16 13:23:13 -0300

    Criando pasta para armazenar os gráficos

--- src/main/java/com/genetico/service/GraficoService.java
+++ src/main/java/com/genetico/service/GraficoService.java
@@ -5,9 +5,14 @@ import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
+import java.io.File;
 import java.io.IOException;
 
 public class GraficoService {
+
+    public GraficoService() {
+        criarPastaGraficos();
+    }
 
     public void gerarGraficoEvolucaoFitness(double[] indicesGeracoes, double[] valoresFitness) {
         if (indicesGeracoes.length != valoresFitness.length) {
@@ -24,15 +29,27 @@ public class GraficoService {
                 .build();
 
         try {
+            var diretorioGraficoFitness = "graficos/evolucao_fitness.png";
             chart.addSeries("Melhor Fitness", indicesGeracoes, valoresFitness).setMarker(SeriesMarkers.CIRCLE);
-            BitmapEncoder.saveBitmap(chart, "evolucao_fitness.png", BitmapEncoder.BitmapFormat.PNG);
+            BitmapEncoder.saveBitmap(chart, diretorioGraficoFitness, BitmapEncoder.BitmapFormat.PNG);
+
+            System.out.println("Gráfico gerado com XChart em: " + diretorioGraficoFitness);
         } catch (IOException e) {
             System.out.println("Houve um erro de entrada e saída " + e.getMessage());
             throw new RuntimeException(e);
         }
 
-        System.out.println("Gráfico gerado com XChart!");
 
         new SwingWrapper<>(chart).displayChart();
+    }
+
+    private void criarPastaGraficos() {
+        var pastaGraficos = new File("graficos");
+
+        if (!pastaGraficos.exists()) {
+            if (!pastaGraficos.mkdirs()) {
+                throw new RuntimeException("Diretório " + pastaGraficos + " não conseguiu ser criado");
+            }
+        }
     }
 }
