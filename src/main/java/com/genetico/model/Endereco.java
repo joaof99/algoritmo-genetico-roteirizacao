4a4f9324396b15af7b712b7143f78c4768dc3795
João <Sem e-mail>
2026-07-12 18:31:39 -0300

    Adiciona record Rota e Endereco

---
+++ src/main/java/com/genetico/model/Endereco.java
@@ -0,0 +1,3 @@
+package com.genetico.model;
+
+public record Endereco(Long id, Double latitude, Double longitude, String descricao) {}
---
+++ src/main/java/com/genetico/model/Rota.java
@@ -0,0 +1,6 @@
+package com.genetico.model;
+
+import java.util.List;
+
+public record Rota(List<Endereco> enderecos) {}
+
