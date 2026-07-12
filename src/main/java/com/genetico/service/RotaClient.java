b4ba3f741920bf062ca424411a60232267cec838
João <Sem e-mail>
2026-07-12 18:32:09 -0300

    Adiciona RotaClient

---
+++ src/main/java/com/genetico/service/RotaClient.java
@@ -0,0 +1,52 @@
+package com.genetico.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.genetico.model.Endereco;
+import com.genetico.model.Rota;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotaClient {
+    private static final String URL_BASE = System.getenv("AG_ADMINISTRATIVO_URL") != null
+            ? System.getenv("AG_ADMINISTRATIVO_URL")
+            : "http://localhost:8080";
+
+    private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public List<Rota> buscarTodasRotas() throws IOException, InterruptedException {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(URL_BASE + "/rotas/todas"))
+                .GET()
+                .build();
+
+        var response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        var json = OBJECT_MAPPER.readTree(response.body());
+
+        var rotas = new ArrayList<Rota>();
+
+        json.forEach(rota -> {
+            var enderecos = new ArrayList<Endereco>();
+
+            rota.get("enderecos").forEach(endereco -> {
+                var id = endereco.get("id").asLong();
+                var latitude = endereco.get("latitude").asDouble();
+                var longitude = endereco.get("longitude").asDouble();
+                var cidade = endereco.get("cidade").asText();
+
+                enderecos.add(new Endereco(id, latitude, longitude, cidade));
+            });
+
+            rotas.add(new Rota(enderecos));
+        });
+
+        return rotas;
+    }
+}
+
