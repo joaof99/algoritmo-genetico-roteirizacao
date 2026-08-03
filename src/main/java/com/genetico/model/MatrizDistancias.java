0b70720744a020584e69e4275da156a2c51b013e
João <Sem e-mail>
2026-08-02 21:48:56 -0300

    Cria classe de matriz de distâncias

---
+++ src/main/java/com/genetico/model/MatrizDistancias.java
@@ -0,0 +1,29 @@
+package com.genetico.model;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.eclipse.collections.impl.map.mutable.primitive.LongDoubleHashMap;
+import org.eclipse.collections.impl.map.mutable.primitive.LongObjectHashMap;
+
+public class MatrizDistancias {
+    private static final Logger log = LogManager.getLogger(MatrizDistancias.class);
+    private final LongObjectHashMap<LongDoubleHashMap> distancias = new LongObjectHashMap<>();
+
+    public void definirDistancia(long idOrigem, long idDestino, double distancia) {
+        log.info("Definindo distância entre os pontos de ID {} e {}", idOrigem, idDestino);
+        distancias.getIfAbsentPut(idOrigem, LongDoubleHashMap::new).put(idDestino, distancia);
+    }
+
+    public double obterDistancia(long idOrigem, long idDestino) {
+        var linha = distancias.get(idOrigem);
+
+        if (linha == null || !linha.containsKey(idDestino)) {
+            var mensagemErro = String.format("Distância não calculada entre pontos de ID %d e %d", idOrigem, idDestino);
+
+            log.error(mensagemErro);
+            throw new IllegalArgumentException(mensagemErro);
+        }
+
+        return linha.get(idDestino);
+    }
+}
