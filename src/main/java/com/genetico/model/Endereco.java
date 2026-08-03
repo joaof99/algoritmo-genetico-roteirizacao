91db09b287ba7aa4fe3c00dd72bdccd4600df13b
João <Sem e-mail>
2026-08-02 21:45:49 -0300

    Implementa hashcode e Equals

--- src/main/java/com/genetico/model/Endereco.java
+++ src/main/java/com/genetico/model/Endereco.java
@@ -1,3 +1,17 @@
 package com.genetico.model;
 
-public record Endereco(Long id, Double latitude, Double longitude, String descricao) {}
+import java.util.Objects;
+
+public record Endereco(Long id, Double latitude, Double longitude, String descricao) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Endereco endereco)) return false;
+        return Objects.equals(id, endereco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
