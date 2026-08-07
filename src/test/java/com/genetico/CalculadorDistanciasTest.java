43d172d442541e9a71ffc0dc01c43405f4ba1d62
João <Sem e-mail>
2026-08-06 23:19:50 -0300

    Inicia teste

--- src/test/java/com/genetico/CalculadorDistanciasTest.java
+++ src/test/java/com/genetico/CalculadorDistanciasTest.java
@@ -1,10 +1,17 @@
 package com.genetico;
 
 import com.genetico.model.Endereco;
+import com.genetico.model.Rota;
+import com.genetico.service.RotaClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CalculadorDistanciasTest {
 
@@ -14,5 +21,18 @@ class CalculadorDistanciasTest {
         var sp = new Endereco(1L, -23.5505, -46.6333, "");
         var rj = new Endereco(2L, -22.9068, -43.1729, "");
         assertEquals(360.74882490989955, CalculadorDistancias.calcularDistanciaHaversine(sp, rj));
+    }
+
+    @Test
+    @DisplayName("Matriz de distâncias deve ser criada corretamente")
+    public void matrizDeDistanciasDeveSerCriadaCorretamente() throws IOException, InterruptedException {
+        var rotaClient = mock(RotaClient.class);
+        var enderecos = List.of(new Endereco(1L, -3.2, -4.5, "Hospital"), new Endereco(3L, -90.3, -50.9, "Barbearia"));
+        var rota = new Rota(enderecos);
+
+        when(rotaClient.buscarTodasRotas()).thenReturn(List.of(rota));
+
+        var calculadorDistancias = new CalculadorDistancias(rotaClient);
+        calculadorDistancias.inicializarDistanciasHaversine();
     }
 }
