eb2836cbd121ebbf87d262955bd7697fecfb2706
João <Sem e-mail>
2026-08-11 22:51:59 -0300

    17 - Deixa var próxima de onde é utilizada

--- src/test/java/com/genetico/CalculadorDistanciasTest.java
+++ src/test/java/com/genetico/CalculadorDistanciasTest.java
@@ -16,8 +16,6 @@ class CalculadorDistanciasTest {
     @Test
     @DisplayName("Matriz de distâncias deve ser criada corretamente")
     public void matrizDeDistanciasDeveSerCriadaCorretamente() {
-        var rotaClient = mock(RotaClient.class);
-
         var hospitalSP = new Endereco(1, -5.3, -8.2, "Hospital SP");
         var barbearia = new Endereco(3, -90.3, -50.9, "Barbearia");
         var shoppingSP  = new Endereco(2, -90.3, -50.9, "Shopping SP");
@@ -30,6 +28,8 @@ class CalculadorDistanciasTest {
         var shoppingSC = new Endereco(5, -90.3, -50.9, "Shopping SC");
 
         var enderecos = List.of(hospitalSP, barbearia, shoppingSP, padaria, armazem, hospitalRJ, pizzaria, bar, estadio, shoppingSC);
+
+        var rotaClient = mock(RotaClient.class);
         when(rotaClient.buscarTodosEnderecos()).thenReturn(enderecos);
 
         CalculadorDistancias.inicializar(rotaClient, new Haversine());
