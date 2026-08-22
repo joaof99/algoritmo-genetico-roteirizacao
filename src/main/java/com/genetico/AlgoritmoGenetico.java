057b62b32129c911ca9a3d5e2ad180ec2a7e6cd4
João <Sem e-mail>
2026-08-21 21:40:16 -0300

    17 - Remove imports desnecessários

--- src/main/java/com/genetico/AlgoritmoGenetico.java
+++ src/main/java/com/genetico/AlgoritmoGenetico.java
@@ -1,11 +1,9 @@
 package com.genetico;
 
-import com.genetico.distancia.CalculadorDistancias;
-import com.genetico.distancia.MetodoCalculoDistancia;
 import com.genetico.exception.RotaClientException;
+import com.genetico.factory.GraficoServiceFactory;
 import com.genetico.model.Populacao;
 import com.genetico.service.GraficoService;
-import com.genetico.factory.GraficoServiceFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
