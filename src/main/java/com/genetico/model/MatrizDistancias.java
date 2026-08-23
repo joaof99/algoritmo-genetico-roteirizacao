ecdddb16ea4fdb2349792a4d3bc78b211ce3b940
João <Sem e-mail>
2026-08-22 22:05:12 -0300

    17 - Ajusta tipagem de map

--- src/main/java/com/genetico/model/MatrizDistancias.java
+++ src/main/java/com/genetico/model/MatrizDistancias.java
@@ -2,23 +2,23 @@ package com.genetico.model;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.eclipse.collections.impl.map.mutable.primitive.LongDoubleHashMap;
-import org.eclipse.collections.impl.map.mutable.primitive.LongObjectHashMap;
+import org.eclipse.collections.impl.map.mutable.primitive.IntDoubleHashMap;
+import org.eclipse.collections.impl.map.mutable.primitive.IntObjectHashMap;
 
 public class MatrizDistancias {
     private static final Logger log = LogManager.getLogger();
-    private static final LongObjectHashMap<LongDoubleHashMap> distancias = new LongObjectHashMap<>();
+    private static final IntObjectHashMap<IntDoubleHashMap> distancias = new IntObjectHashMap<>();
 
     private MatrizDistancias(){
 
     }
 
-    public static void setDistancia(long idOrigem, long idDestino, double distancia) {
+    public static void setDistancia(int idOrigem, int idDestino, double distancia) {
         log.info("Definindo distância entre os pontos de ID {} e ID {}", idOrigem, idDestino);
-        distancias.getIfAbsentPut(idOrigem, LongDoubleHashMap::new).put(idDestino, distancia);
+        distancias.getIfAbsentPut(idOrigem, IntDoubleHashMap::new).put(idDestino, distancia);
     }
 
-    public static double getDistancia(long idOrigem, long idDestino) {
+    public static double getDistancia(int idOrigem, int idDestino) {
         var linha = distancias.get(idOrigem);
 
         if (linha == null || !linha.containsKey(idDestino)) {
@@ -34,7 +34,7 @@ public class MatrizDistancias {
     public static int getQuantidadeDistancias() {
         return distancias.values()
                 .stream()
-                .mapToInt(LongDoubleHashMap::size)
+                .mapToInt(IntDoubleHashMap::size)
                 .sum();
     }
 }
